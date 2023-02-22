f7fe7bfe278ecb877e522fc7ed71dc63d48ef791
Simon Nyholm Sørensen <simonnyholm@htomail>
2023-02-22 22:21:02 +0100

    add cleanup, routes and navigation

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+
+import "./App.css";
+import Home from "./views/Home.jsx";
+import About from "./views/About.jsx";
+import NewItem from "./views/NewItem.jsx";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/new" element={<NewItem />} />
+      </Route>
+    </Routes>
   );
 }
 
---
+++ src/views/Home.jsx
@@ -0,0 +1,11 @@
+const Home = () => {
+  return (
+    <div>
+      <section>
+        <h1>Heading</h1>
+      </section>
+    </div>
+  );
+};
+
+export default Home;
